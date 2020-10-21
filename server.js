74f79cc14dc9b561d73b4b599d9cc0b59c650799
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-10-21 06:31:06 -0400

    updated port code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/expense-tracker", {
+mongoose.connect(process.env.PORT || "mongodb://localhost/expense-tracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
