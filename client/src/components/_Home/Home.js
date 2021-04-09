4959c95ce2bbd017e6a150276f9deeaf55afd95a
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2021-04-09 18:50:18 -0400

    Got hero section to a good point on the Home page.

--- client/src/components/_Home/Home.js
+++ client/src/components/_Home/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import './Home.css';
 
 export default function Home() {
     return (
         <div>
-            Home page goes here...
+            <header className="home-hero">
+                
+                <div className="header-overlay">
+                    <h1 className="home-header">EXPENSE TRACKER</h1>
+                    <div className="rotate-text">
+                        <span className="text-1">Track Expenses</span>
+                        <span className="text-2">Save Money</span>
+                        <span className="text-3">Buy Nicer Things</span>
+                    </div>
+                </div>
+            </header>
+
         </div>
     )
 }
